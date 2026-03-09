86299b898c8ecaf5a364658ee38beadd5e955e42
bspoloo <animetx098@gmail.com>
2026-03-09 04:33:23 -0400

    Added Metric3D

---
+++ Metric3D/functions/graphic_comparation.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import cv2
+
+def graphic_comparation(raw_img, depth):
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
+    plt.imshow(cv2.cvtColor(raw_img, cv2.COLOR_BGR2RGB))
+    plt.title("Imagen original")
+    plt.axis("off")
+
+    plt.subplot(1,2,2)
+    # plt.imshow(depth, cmap='gray')
+    plt.imshow(depth)
+    plt.title("Mapa de profundidad")
+    plt.axis("off")
+
+    plt.show()
